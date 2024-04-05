2fb5f5222288c5fab203360775a0974cab717531
Julian Moreno <juliancastillo204@gmail.com>
2024-04-05 11:33:25 -0600

    registro de usuario y logeo

--- CumpleAgenda/Screens/Login.jsx
+++ CumpleAgenda/Screens/Login.jsx
@@ -1,5 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, TextInput,TouchableOpacity, Button } from 'react-native';
+//asynStorage
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { useEffect, useState } from 'react';
@@ -9,23 +12,51 @@ import SingUp from './SingUp';
 
 export default function Login({navigation}) {
 
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
+  const handleLogin = async()=>{
+    try {
+       // Obtener los datos del usuario utilizando el correo electrónico como clave
+       const userData = await AsyncStorage.getItem(username);
+      if (userData) {
+        const user = JSON.parse(userData);
+         if (user.password === password) {
+           navigation.navigate('Salir');
+          } else { // Aquí estaba el error de sintaxis
+             alert('Error: Contraseña incorrecta');
+          }
+
+      } else {
+        alert('Error: Usuario no encontrado');
+      }
+    } catch (error) {
+      console.log('Error:', error);
+      alert('Error: Ocurrió un error al iniciar sesión');
+    }
+  };
+
+  
   return(
     <View style={styles.container}>
       <Text style={styles.title} >AGENDA</Text>
       <TextInput
         style={styles.input}
+        value={username}
+        onChangeText={text => setUsername(text)}
         placeholder='Usuario'
       />
       <TextInput
         style={styles.input}
         placeholder='Contraseña'
+        value={password}
+        onChangeText={text => setPassword(text)}
         secureTextEntry={true}
       />
-      <TouchableOpacity>
-      <Button style={styles.btnLogin} title='INGRESAR' onPress={()=>navigation.navigate("Salir")} ></Button>
-
+      <TouchableOpacity style={styles.btnLogin} onPress={handleLogin}>
+        <Text style={styles.btnText}>INGRESAR</Text>
       </TouchableOpacity>
+
 
 
       <View style={styles.container_CrearCuenta}>
@@ -65,5 +96,13 @@ const styles = StyleSheet.create({
   container_CrearCuenta:{
     marginTop: '50%',
     borderRadius:20,
+  },
+  btnText:{
+    width:'80%',
+    paddingHorizontal:50,
+    paddingVertical:10,
+    borderRadius:12,
+    backgroundColor:'blue',
+    color:'white'
   }
 });
--- CumpleAgenda/Screens/SingUp.jsx
+++ CumpleAgenda/Screens/SingUp.jsx
@@ -1,39 +1,88 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, TextInput, Button, TouchableOpacity} from 'react-native';
-
+//asyncStorage
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-export default function SingUp({navigation}) {
+import { useEffect, useState } from 'react';
+export default function SingUp({ navigation }) {
+  //variables para el registro
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const handleSignUp = async () => {
+    //validamos que las dos contrasenas sean iguales
+    if (password !== confirmPassword) {
+      alert('la contraseña no coincide, intente de nuevo');
+      return;
+    }
+
+    //guaramos los datos del usuario en AsynStorage
+    try {
+
+      // Comprobar si ya existe un usuario con la misma dirección de correo electrónico
+      const existingUser = await AsyncStorage.getItem(email);
+      if (existingUser) {
+        alert('Ya existe un usuario con esta dirección de correo electrónico');
+        return;
+      }
+      // Guardar los datos del usuario en AsyncStorage
+      const userData = JSON.stringify({ username, email, password });
+
+      await AsyncStorage.setItem(username, userData);
+      alert('Usuario Creado Correctamente');
+      //Si todo es correcto 
+      // Redirigimos a la pantalla de inicio de sesión
+      navigation.goBack();
+
+
+    } catch (error) {
+      console.log("error al guardar los datos " + error)
+      alert("error en el servidor intente mas tarde");
+    }
+
+  }
+
   return (
     <View style={styles.container}>
 
       <Text style={styles.title}>REGISTRATE</Text>
       <TextInput
         style={styles.input}
+        value={username}
         placeholder='Usuario'
+        onChangeText={text => setUsername(text)}
       />
       <TextInput
         style={styles.input}
+        value={email}
         placeholder='Correo'
+        onChangeText={text => setEmail(text)}
       />
-       <TextInput
+      <TextInput
         style={styles.input}
+        value={password}
         placeholder='Contraseña'
         secureTextEntry={true}
+        onChangeText={text => setPassword(text)}
       />
-      
-       <TextInput
+
+      <TextInput
         style={styles.input}
-        placeholder='Contraseña'
+        value={confirmPassword}
+        placeholder=' Repite la Contraseña'
         secureTextEntry={true}
+        onChangeText={text => setConfirmPassword(text)}
       />
 
-    <TouchableOpacity>
-      <Button title='CREAR CUENTA' onPress={() => navigation.goBack()} style={styles.btnCrearCuenta} ></Button>
+      <TouchableOpacity>
+        <Button title='CREAR CUENTA' onPress={handleSignUp} style={styles.btnCrearCuenta} ></Button>
 
-    </TouchableOpacity>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -45,13 +94,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  title:{
-    fontSize:40,
-    marginBottom:50,
-    color:"blue",
-    fontFamily:'Cathylise',
+  title: {
+    fontSize: 40,
+    marginBottom: 50,
+    color: "blue",
+    fontFamily: 'Cathylise',
 
-  },input:{
+  }, input: {
     width: '80%',
     height: 40,
     borderWidth: 1,
@@ -60,8 +109,8 @@ const styles = StyleSheet.create({
     marginBottom: 20,
     paddingHorizontal: 10,
   },
-  btnCrearCuenta:{
-    width:'80%'
+  btnCrearCuenta: {
+    width: '80%'
   }
-  
+
 });
